a82819e372c20a4d4b5e1d7fb7e2dc1a9a7bb957
vpanic <vpanic00@estudiantes.unileon.es>
2018-04-06 16:11:16 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Date fecha =new Date(12,5,2018);
+		System.out.println(fecha.isSameDay(12));
+		System.out.println(fecha.isSameMonth(5));
+		System.out.println(fecha.isSameYear(2018));
+		System.out.println(fecha.isSame(fecha));
+		fecha.printsMonth();
+		fecha.dayToMonthEnd(fecha);
+		fecha.daySinceFirstDayOfYear(fecha);
+		fecha.intentsHitDateDoWhile();
+		fecha.intentsHitDateWhile();
+		fecha.remainingMonths(fecha);
+		fecha.printDate(fecha);
+		fecha.theSameMesDay(12);
+		System.out.println(fecha.correctDate(12, 2,2000));
+		fecha.season(2);
+		fecha.knowDayIs(fecha, 5);
+		System.out.println(fecha.passToString(11));
+		
+		
+	}
+
+}
