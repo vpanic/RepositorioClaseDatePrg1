bff471ec32f3b9930521f585bec5da292bfaa685
vpanic <vpanic00@estudiantes.unileon.es>
2018-04-16 17:24:51 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -7,16 +8,16 @@ public class Main {
 		System.out.println(fecha.isSameMonth(5));
 		System.out.println(fecha.isSameYear(2018));
 		System.out.println(fecha.isSame(fecha));
-		fecha.printsMonth();
+		
 		fecha.dayToMonthEnd(fecha);
 		fecha.daySinceFirstDayOfYear(fecha);
 		fecha.intentsHitDateDoWhile();
 		fecha.intentsHitDateWhile();
 		fecha.remainingMonths(fecha);
-		fecha.printDate(fecha);
-		fecha.theSameMesDay(12);
+		
+		
 		System.out.println(fecha.correctDate(12, 2,2000));
-		fecha.season(2);
+		
 		fecha.knowDayIs(fecha, 5);
 		System.out.println(fecha.passToString(11));
 		
